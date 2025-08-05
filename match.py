dce54bd838d00ee4b7b34f0a85689ed69bcdb428
Retostephen <retostephen2006@gmail.com>
2025-08-05 14:13:25 +0100

    Did some basic understandings on how to use the match condition

---
+++ match.py
@@ -0,0 +1,12 @@
+colour = "black"
+match colour:
+	case "blue":
+		print("blue")
+	case "black":
+		print("black")
+	case "pink":
+		print("pink")
+	case "green":
+		print("green")
+	case _:
+		print("Neutral")
